196eb500fd8b82ef6bfb3730154a85f662ddf84a
umerusman790 <56084277+umerusman790@users.noreply.github.com>
2020-08-11 06:26:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("Hello guys")
+html_temp = """
+    <div style="background-color:orange;padding:10px">
+    <h1 style="color:white;text-align:center;">Titanic Crash APP </h1>
+    </div>
+    """
+st.markdown(html_temp,unsafe_allow_html=True)
+
+html_temp = """
+    <div style="background-color:orange;padding:10px">
+    <h1 style="color:white;text-align:center;">FA17-BSE-C-132</h1>
+    </div>
+    """
+st.sidebar.markdown(html_temp,unsafe_allow_html=True)
+
+
+
+
+data_set_name = st.sidebar.selectbox('Choose Dataset' ,
+                                     ('Titanic Crash','Not Available'))
+
+classifier_name = st.sidebar.selectbox('Choose Algorithm' ,
+                                       ('Knn','Svm','Random Forest'))
